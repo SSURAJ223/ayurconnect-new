34b98d26d4fbca6e14adc550e781b9b52c6583e5
SSURAJ223 <ssuraj.amz@gmail.com>
2025-07-13 15:29:35 +0530

    Create LeafIcon.tsx

---
+++ components/icons/LeafIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const LeafIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+    <path d="M11 20A7 7 0 0 1 4 13V8a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v5a7 7 0 0 1-7 7Zm-6-16V1a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3" />
+  </svg>
+);

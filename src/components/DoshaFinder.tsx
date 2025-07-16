5cfd65d529a7188b257934bc2138270c970b44c7
SSURAJ223 <ssuraj.amz@gmail.com>
2025-07-16 23:32:46 +0530

    Update DoshaFinder.tsx

--- src/components/DoshaFinder.tsx
+++ src/components/DoshaFinder.tsx
@@ -1,10 +1,11 @@
+
 import React, { useState, useCallback, useMemo, useRef, useEffect } from 'react';
 import { getDoshaAnalysis } from '../services/geminiService';
 import type { DoshaAnalysisResult } from '../types';
 import { Spinner } from './Spinner';
-import { UserCircleIcon } from './icons/UserCircleIcon.tsx';
+import { UserCircleIcon } from './icons/UserCircleIcon';
 import { LifestyleCard } from './LifestyleCard';
-import { DoshaResultCard } from './DoshaResultCard.tsx';
+import { DoshaResultCard } from './DoshaResultCard';
 import { ShareButton } from './ShareButton';
 import { BookOpenIcon } from './icons/BookOpenIcon';
 

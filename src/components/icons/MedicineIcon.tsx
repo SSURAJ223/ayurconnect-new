956abb65224bf0d960ca5cb4cbfe411ea9c29bc8
SSURAJ223 <ssuraj.amz@gmail.com>
2025-07-27 00:27:47 +0530

    Create MedicineIcon.tsx

---
+++ src/components/icons/MedicineIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const MedicineIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+    <path d="M20 21 7.5 8.5"/>
+    <path d="M10 16H6v-4h4v4Z"/>
+    <path d="M14.5 3.5c2.8 2.8 2.8 7.2 0 10"/>
+    <path d="M21 10.5c-2.8-2.8-7.2-2.8-10 0"/>
+  </svg>
+);

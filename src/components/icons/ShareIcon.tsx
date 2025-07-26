81b21d6218e31eea70d808e372667d390cbaab3c
SSURAJ223 <ssuraj.amz@gmail.com>
2025-07-26 17:59:48 +0530

    Update ShareIcon.tsx

--- src/components/icons/ShareIcon.tsx
+++ src/components/icons/ShareIcon.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export const SearchIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+export const ShareIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-    <circle cx="11" cy="11" r="8" />
-    <line x1="21" y1="21" x2="16.65" y2="16.65" />
+    <circle cx="18" cy="5" r="3"></circle>
+    <circle cx="6" cy="12" r="3"></circle>
+    <circle cx="18" cy="19" r="3"></circle>
+    <line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line>
+    <line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line>
   </svg>
 );
-
